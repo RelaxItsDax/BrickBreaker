9236290b732226b745fb92ff3fb0083e20dac5c0
RelaxItsDax <relaxitsdax@gmail.com>
2023-04-12 07:50:56 -0400

    but heres the breaker!

---
+++ src/main/java/me/relaxitsdax/breaker/Breaker.java
@@ -0,0 +1,17 @@
+package me.relaxitsdax.breaker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Breaker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
